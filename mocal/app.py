767facbafea7ae79268029b643289b777512c4c9
haner27 <369685930@qq.com>
2015-07-09 14:35:19 +0800

    add restful

--- mocal/app.py
+++ mocal/app.py
@@ -1,10 +1,16 @@
 # -*- coding: utf8 -*-
 
+import os
+import glob
+
+from utils.logger import logger
+
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
-
+from flask_cache import Cache
+from flask_restful import Api, Resource
 
 app = Flask('mocal')
 
@@ -22,6 +28,7 @@ class DataBase(SQLAlchemy):
 
 db = DataBase(app)
 
+
 # email
 app.config['MAIL_SERVER'] = 'smtp.126.com'
 app.config['MAIL_PORT'] = 25
@@ -36,4 +43,44 @@ app.config['MAIL_SUPPRESS_SEND'] = app.testing
 app.config['MAIL_ASCII_ATTACHMENTS'] = True
 mail = Mail(app) # 初始化mail
 
+
+# cache
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
+
+
+# config blueprint
+def config_blueprint(application):
+    views = 'views'
+    register_blueprints = []
+
+    for py_file in glob.glob(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'views', '*.py')):
+        view_name = os.path.basename(py_file)[:-3]
+        if view_name == '__init__':
+            continue
+
+        instance = import_instance('.'.join([views, view_name, 'instance']))
+        if instance:
+            register_blueprints.append(instance)
+            logger.info('{0} import instance succeed'.format(view_name))
+        else:
+            logger.error('{0} import instance failed'.format(view_name))
+
+    for blue_print in register_blueprints:
+        application.register_blueprint(blue_print)
+
+
+def import_instance(instance_name):
+    package = instance_name[:instance_name.rindex('.')]
+
+    module = __import__(package, fromlist=['instance'])
+    if hasattr(module, 'instance'):
+        instance = getattr(module, 'instance')
+        return instance
+
+    return None
+
+config_blueprint(app)
+
+
+# get m_app
 m_app = app
---
+++ mocal/views/user.py
@@ -0,0 +1,12 @@
+# -*- coding: utf8 -*-
+
+from flask import Blueprint, render_template, url_for
+from flask.views import MethodView
+from mocal.views import register_view
+
+instance = Blueprint('user', __name__)
+
+@register_view('/user', instance, ['get'])
+class User(MethodView):
+    def get(self):
+        return render_template('index.html')
