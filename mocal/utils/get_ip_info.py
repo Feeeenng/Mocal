6d186cff8a239ce332d0858fafc6e4e7f58d3a52
haner27 <369685930@qq.com>
2016-01-18 12:17:16 +0800

    print res

--- mocal/utils/get_ip_info.py
+++ mocal/utils/get_ip_info.py
@@ -8,7 +8,7 @@ def get_ip_city_by_ip(ip_address):
     if ip_address == '127.0.0.1':
         city = '北京市'  # 默认
     else:
-        res = get_ip_info(ip_address)
+        res = get_ip_info(ip_address).get('data')
         city = res.get('city')
     return city
 
