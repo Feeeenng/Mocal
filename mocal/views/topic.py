fa1a8c0c8f98247bec3b4311e2cdb91bab4dba30
haner27 <369685930@qq.com>
2016-03-28 19:18:40 +0800

    topics页面

--- mocal/views/topic.py
+++ mocal/views/topic.py
@@ -11,24 +11,18 @@ instance = Blueprint('topic', __name__)
 
 
 @instance.before_request
+@login_required
 def before_request():
     pass
 
 
 @instance.route('/topic', methods=['GET'])
-@login_required
 def topic_index():
     topic_id = request.args.get('topic_id', 0, int)
     topic = Topic.from_db(id=topic_id)
     if topic:
-        if not topic.is_marked(uid=current_user.id):
-            ut = UserTopics(uid=current_user.id, tid=topic_id)
-            ut.save(add=True)
-        else:
-            ut = UserTopics.from_db(uid=current_user.id, tid=topic_id)
-            ut.deleted_at = None
-            ut.save()
-
+        if not topic.is_marked(current_user.id):
+            return jsonify(success=False, errors='请先关注话题！')
         messages = Message.fetch(group=topic_id, page=1, count=10)
         creator = User.from_db(id=topic.creator_id)
         return render_template('topic/talk.html', messages=messages, topic=topic, creator_name=creator.nickname,
@@ -38,7 +32,6 @@ def topic_index():
 
 
 @instance.route('/topics', methods=['GET'])
-@login_required
 def topics():
     page = request.args.get('page', 1, int)
     topics = Topic.fetch(page=page, count=8)
@@ -53,3 +46,47 @@ def get_user_name(uid):
 def get_user_photo(uid):
     user = User.from_db(id=uid)
     return user.photo
+
+
+@instance.route('/topic/mark', methods=['POST'])
+def mark():
+    res = request.get_json(force=True)
+    topic_id = res.get('topic_id')
+    ut = UserTopics.from_db(uid=current_user.id, tid=topic_id)
+    if ut:
+        if ut.deleted_at:
+            ut.deleted_at = None
+            ut.save()
+
+    else:
+        ut = UserTopics(uid=current_user.id, tid=topic_id)
+        ut.save(True)
+
+    tp = Topic.from_id(topic_id)
+    if tp:
+        return jsonify(success=True, is_marked=tp.is_marked(current_user.id))
+    return jsonify(success=False)
+
+
+@instance.route('/topic/cancel', methods=['POST'])
+def cancel():
+    res = request.get_json(force=True)
+    topic_id = res.get('topic_id')
+    ut = UserTopics.from_db(uid=current_user.id, tid=topic_id)
+    if ut:
+        ut.cancel()
+        ut.save()
+    tp = Topic.from_id(topic_id)
+    if tp:
+        return jsonify(success=True, is_marked=tp.is_marked(current_user.id))
+    return jsonify(success=False)
+
+
+@instance.route('/topic/is_marked', methods=['POST'])
+def is_marked():
+    res = request.get_json(force=True)
+    topic_id = res.get('topic_id')
+    tp = Topic.from_db(id=topic_id)
+    if tp:
+        return jsonify(success=True, is_marked=tp.is_marked(current_user.id))
+    return jsonify(success=False)
