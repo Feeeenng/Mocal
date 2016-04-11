a42dd0c01e75c7265c55e561cf533210099f5fc5
haner27 <369685930@qq.com>
2016-04-11 19:07:15 +0800

    ss

--- mocal/views/mian.py
+++ mocal/views/mian.py
@@ -15,16 +15,14 @@ def index():
     # 轮播图
 
     # 天气
-    from mocal.utils.get_weather_info import get_weather_city_info
-    from mocal.utils.get_ip_info import get_ip_city_by_ip
-    ip = request.remote_addr  # 获取访问ip地址
-    city = get_ip_city_by_ip(ip)  # 根据ip地址获取城市名
-    success, weather_info = get_weather_city_info(city)  # 根据城市名获取天气状况
-    results = weather_info['results'][0]
-    date = weather_info['date']
-    dates = [(datetime.strptime(date, '%Y-%m-%d') + timedelta(days=i)).strftime('%m月%d日')
-             for i in [0, 1, 2, 3]]
-    year = datetime.now().year
-    return render_template('index.html', title='Mocal', index=results['index'],
-                           current_city=results['currentCity'], weather_data=results['weather_data'], dates=dates,
-                           year=year)
+    # from mocal.utils.get_weather_info import get_weather_city_info
+    # from mocal.utils.get_ip_info import get_ip_city_by_ip
+    # ip = request.remote_addr  # 获取访问ip地址
+    # city = get_ip_city_by_ip(ip)  # 根据ip地址获取城市名
+    # success, weather_info = get_weather_city_info(city)  # 根据城市名获取天气状况
+    # results = weather_info['results'][0]
+    # date = weather_info['date']
+    # dates = [(datetime.strptime(date, '%Y-%m-%d') + timedelta(days=i)).strftime('%m月%d日')
+    #          for i in [0, 1, 2, 3]]
+    # year = datetime.now().year
+    return render_template('index.html', title='Mocal')
